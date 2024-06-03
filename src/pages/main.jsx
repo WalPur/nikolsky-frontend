0fccefbd9d78076f860531c1e22f4eb063a036e9
WalPur <mishkamba1@gmail.com>
2024-06-03 23:11:07 +0900

    progress bar is complete

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -36,15 +36,39 @@ function PageMain() {
         <PostNavigation />
       </div>
       <div className="w-full h-px border-2 border-slate-700"></div>
-      <div className="w-full flex justify-end">
+      <div className="w-full flex justify-end items-center gap-5">
         <div className="text-black text-3xl font-medium font-['Alegreya']">
           Прокрутите, чтобы узнать больше
         </div>
-        <progress
-          value={progress}
-          id="progress"
-          // className="h-1 text-black appearance-none bg-repeat-x bg-progress bg-[auto 3px]"
-        ></progress>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={`size-6 ${progress > 0 ? "" : "opacity-30"}`}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+        <progress value={progress} id="progress"></progress>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={`size-6 ${progress == 1 ? "opacity-30" : ""}`}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+          />
+        </svg>
       </div>
     </div>
   );
